7cf4af6b0ebe55f5f921c5621f3f86b8efd7338e
Joongi Hong <tomotherapy@outlook.com>
2024-06-08 03:57:53 +0900

    a

---
+++ Hitomi-Viewer/Hitomi-Viewer/Cores/CustomControls/TileUserControl.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hitomi_Viewer.Cores.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for TileUserControl.xaml
+    /// </summary>
+    public partial class TileUserControl : UserControl
+    {
+        private PreviewWindow mPreviewWindow;
+
+        public TileUserControl(BitmapSource image, string title, string author, string number, int pages)
+        {
+            InitializeComponent();
+
+            Cover_Image.Source = image;
+            Title_TextBlock.Text = title;
+            Author_TextBlock.Text = author;
+            Number_TextBlock.Text = number;
+            Pages_TextBlock.Text = $"p{pages}";
+
+            mPreviewWindow = new PreviewWindow();
+        }
+
+        private void Grid_MouseEnter(object sender, MouseEventArgs e)
+        {
+            mPreviewWindow.Visibility = Visibility.Visible;
+            mPreviewWindow.DragMove();
+        }
+
+        private void Grid_MouseLeave(object sender, MouseEventArgs e)
+        {
+            mPreviewWindow.Visibility = Visibility.Visible;
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void A_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+        }
+
+        private void B_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void C_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Canvas_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Down)
+            {
+                Canvas.SetTop(DogeIcon, Canvas.GetTop(DogeIcon) + 10);
+            }
+            else if (e.Key == Key.Up)
+            {
+                Canvas.SetTop(DogeIcon, Canvas.GetTop(DogeIcon) - 10);
+            }
+            else if (e.Key == Key.Left)
+            {
+                Canvas.SetLeft(DogeIcon, Canvas.GetLeft(DogeIcon) - 10);
+            }
+            else if (e.Key == Key.Right)
+            {
+                Canvas.SetLeft(DogeIcon, Canvas.GetLeft(DogeIcon) + 10);
+            }
+        }
+
+        private void RectangleMouseEnter(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void MyCanvas_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            Canvas_KeyDown(sender, e);
+        }
+    }
+}
